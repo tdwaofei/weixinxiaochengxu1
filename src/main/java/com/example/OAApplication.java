a1350cf3ef3441f70fe6f3a44d4ef243dc7ccb58
tdwaofei <fei_ao@163.com>
2025-02-26 17:46:19 +0800

    只有一个主类

--- src/main/java/com/example/OAApplication.java
+++ src/main/java/com/example/OAApplication.java
@@ -1,11 +1,11 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example")
+@MapperScan("com.example.mapper")
 public class OAApplication {
     public static void main(String[] args) {
         SpringApplication.run(OAApplication.class, args);
