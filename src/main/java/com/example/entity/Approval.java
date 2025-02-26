fec5fcffcabbe0f2226980d39f35228c6fe53dd1
tdwaofei <fei_ao@163.com>
2025-02-26 17:29:36 +0800

    移除了所有JPA相关的代码

--- src/main/java/com/example/entity/Approval.java
+++ src/main/java/com/example/entity/Approval.java
@@ -1,24 +1,14 @@
 package com.example.entity;
 
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
-@Entity
-@Table(name = "approvals")
 public class Approval {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     private Long applicationId;
     private String approverId;
     private String approvalResult;
     private String approvalComments;
-    
-    @CreationTimestamp
     private LocalDateTime createTime;
 } 
--- src/main/java/com/example/repository/ApprovalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Approval;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApprovalRepository extends JpaRepository<Approval, Long> {
-} 
